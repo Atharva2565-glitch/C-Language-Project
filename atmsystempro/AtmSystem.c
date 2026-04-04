f9ef8a36c3010854492181746de58097dc7021b5
Atharva Suryawanshi <suryawanshiatharva623@gmail.com>
2026-04-04 16:23:53 +0530

    Refactor ATM system code for clarity and structure

--- atmsystempro/AtmSystem.c
+++ atmsystempro/AtmSystem.c
@@ -19,64 +19,64 @@ int main()
 	
 	do{
 		printf("\n ----Atm Menu---- \n");
-			printf("1. Check Balance\n");
-				printf("2. Deposit\n");
-					printf("3. Withdraw\n");
-						printf("4. History\n");
-							printf("5. Exit\n");
+		printf("1. Check Balance\n");
+		printf("2. Deposit\n");
+		printf("3. Withdraw\n");
+		printf("4. History\n");
+		printf("5. Exit\n");
 							
-							printf("Enter the Choice\n");
-							scanf("%d",&choice);
+		printf("Enter the Choice\n");
+		scanf("%d",&choice);
 							
-							switch(choice){
+		switch(choice){
 								
-								case 1:
-									printf("Balance :%.2f\n",balance);
-									sprintf(history[count++],"check Balance  %.2f",balance);
-									break;
+		case 1:
+			   printf("Balance :%.2f\n",balance);
+				sprintf(history[count++],"check Balance  %.2f",balance);
+					break;
 									
-								case 2:
-								    printf("Enter the amount of Deposit :\n");
-									scanf("%f",&amount);
-									balance+=amount;
-									printf("Deposit Successfull!\n");
-									sprintf(history[count++],"Deposited: %.2f",deposit);
-									break;
+		case 2:
+		       printf("Enter the amount of Deposit :\n");
+		    	scanf("%f",&amount);
+				balance+=amount;
+				printf("Deposit Successfull!\n");
+				sprintf(history[count++],"Deposited: %.2f",deposit);
+				break;
 									
-									case 3:
-									printf("Enter the Amount to withdraw :\n");
-									scanf("%f",&amount);
+	    case 3:
+		       printf("Enter the Amount to withdraw :\n");
+		       scanf("%f",&amount);
 									
-									if(amount<=balance){
-										balance-=amount;
-										printf("withdrawal Successful!\n");
-										sprintf(history[count++],"Withdraw  %.2f",withdraw);
-									}
-									else{
-                                           printf("Insufficient Balance!\n");
-                                        }
-                                       break;
+		if(amount<=balance){
+		balance-=amount;
+		printf("withdrawal Successful!\n");
+		sprintf(history[count++],"Withdraw  %.2f",withdraw);
+		}
+		else{
+               printf("Insufficient Balance!\n");
+          }
+              break;
                                        
-									   case 4:
-                                       printf("\n--- Transaction History ---\n");
-                                        if(count == 0) {
-                                        printf("No transactions yet.\n");
-                                         }         
-								          else {
-                                         for( i = 0; i < count; i++) {
-                                         printf("%d. %s\n", i+1, history[i]);
-                                     }
-                                 }
-                                 break;	
+		 case 4:
+                 printf("\n--- Transaction History ---\n");
+                   if(count == 0) {
+                     printf("No transactions yet.\n");
+                      }         
+					  else {
+                        for( i = 0; i < count; i++) {
+                         printf("%d. %s\n", i+1, history[i]);
+                              }
+                             }
+             break;	
                                  
-                                 case 5:
-                                 	printf("Thanku for using ATM !\n");
-                                 	break;
+          case 5:
+                 printf("Thanku for using ATM !\n");
+                break;
                                  	
-                                 	default:
-                                 		printf("Enter the invalid Choice!\n");
-							}							
-	}
+                  default:
+                  printf("Enter the invalid Choice!\n");
+}							
+}
 	while(choice != 5);
 	
 	return 0;
